a2b3c630a69d3c79bee432a4d1fe3e40ae05a148
Marc Wiese <marc@wiese.xyz>
2020-12-15 18:17:04 +0100

    init

---
+++ src/main/java/com/vamarc/simpleboss/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.vamarc.simpleboss;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class HelloApplication extends Application {
+
+}
