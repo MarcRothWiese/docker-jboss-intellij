60e10fa48dfa1ca91ace4831160f4220a58c5f28
Marc Wiese <Marc@wiese.xyz>
2020-12-16 19:05:44 +0100

    Update docker file and web.xml

--- src/main/java/com/vamarc/simpleboss/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.vamarc.simpleboss;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class HelloApplication extends Application {
-
-}
